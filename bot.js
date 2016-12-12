f980281afdeeee29662b24c4c565011c08e08fad
tariqbenezza <tariq.benezza@gmail.com>
2016-12-12 18:53:18 +0100

    add the port number

--- bot.js
+++ bot.js
@@ -24,3 +24,8 @@ stream.on('tweet', function (tweet) {
       console.log(data)
   });
 })
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
