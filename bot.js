42bc029a58e1cd9925aa004c08e2b108919c7d9a
tariqbenezza <tariq.benezza@gmail.com>
2016-12-12 18:11:08 +0100

    First Commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+var Twit = require('twit')
+
+var T = new Twit({
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER.SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET,
+});
+
+// ecoute tous les tweet qui contiennent 'connard'
+var stream = T.stream('statuses/filter', { track: ['#cogedim', 'cogedim', '#altarea', 'altarea'] })
+
+// a chaque tweet
+stream.on('tweet', function (tweet) {
+  //console.log('@'+ tweet.user.screen_name + ' a dit : ' + tweet.text);
+  var user = tweet.user.screen_name;
+  var reponse = {
+    status : '@'+user+' 5 ans et les réserves ne sont toujours pas levées. Fuyez ce promoteur qui vous oublie une fois le dernier chèque remis.',
+    in_reply_to_status_id: tweet.id_str
+  };
+  //console.log('ON REPOND:' + reponse)
+
+  T.post('statuses/update', reponse, function(err, data, response) {
+      console.log(data)
+  });
+})
